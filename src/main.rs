c92ff1e47782891e27350d23d10f4454c9a78e3c
TheMarkum <mhl@haukelid.dk>
2024-01-03 18:17:06 +0100

    Implementaion of fence

--- src/main.rs
+++ src/main.rs
@@ -75,6 +75,7 @@ struct App {
     instance: Instance,
     data: AppData,
     device: Device,
+    frame: usize,
 }
 
 impl App {
@@ -108,25 +109,45 @@ impl App {
             instance,
             data,
             device,
+            frame: 0,
         })
     }
 
     /// Renders a frame for our Vulkan app.
     unsafe fn render(&mut self, window: &Window) -> Result<()> {
+        self.device
+            .wait_for_fences(&[self.data.in_flight_fences[self.frame]], true, u64::MAX)?;
+
+        self.device
+            .reset_fences(&[self.data.in_flight_fences[self.frame]])?;
+
         let image_index = self
             .device
             .acquire_next_image_khr(
                 self.data.swapchain,
                 u64::MAX,
-                self.data.image_available_semaphore,
+                self.data.image_available_semaphores[self.frame],
                 vk::Fence::null(),
             )?
             .0 as usize;
 
-        let wait_semaphores = &[self.data.image_available_semaphore];
+        if !self.data.images_in_flight[image_index as usize].is_null() {
+            self.device.wait_for_fences(
+                &[self.data.images_in_flight[image_index as usize]],
+                true,
+                u64::MAX,
+            )?;
+        }
+
+        self.data.images_in_flight[image_index as usize] = self.data.in_flight_fences[self.frame];
+
+        let wait_semaphores = &[self.data.image_available_semaphores[self.frame]];
+
         let wait_stages = &[vk::PipelineStageFlags::COLOR_ATTACHMENT_OUTPUT];
         let command_buffers = &[self.data.command_buffers[image_index as usize]];
-        let signal_semaphores = &[self.data.render_finished_semaphore];
+
+        let signal_semaphores = &[self.data.render_finished_semaphores[self.frame]];
+
         let submit_info = vk::SubmitInfo::builder()
             .wait_semaphores(wait_semaphores)
             .wait_dst_stage_mask(wait_stages)
@@ -134,7 +155,13 @@ impl App {
             .signal_semaphores(signal_semaphores);
 
         self.device
-            .queue_submit(self.data.graphics_queue, &[submit_info], vk::Fence::null())?;
+            .reset_fences(&[self.data.in_flight_fences[self.frame]])?;
+
+        self.device.queue_submit(
+            self.data.graphics_queue,
+            &[submit_info],
+            self.data.in_flight_fences[self.frame],
+        )?;
 
         let swapchains = &[self.data.swapchain];
         let image_indices = &[image_index as u32];
@@ -146,17 +173,26 @@ impl App {
         self.device
             .queue_present_khr(self.data.present_queue, &present_info)?;
 
-        self.device.queue_wait_idle(self.data.present_queue)?;
+        self.frame = (self.frame + 1) % drawing::render::MAX_FRAMES_IN_FLIGHT;
 
         Ok(())
     }
 
     /// Destroys our Vulkan app.
     unsafe fn destroy(&mut self) {
-        self.device
-            .destroy_semaphore(self.data.render_finished_semaphore, None);
-        self.device
-            .destroy_semaphore(self.data.image_available_semaphore, None);
+        self.data
+            .in_flight_fences
+            .iter()
+            .for_each(|f| self.device.destroy_fence(*f, None));
+
+        self.data
+            .render_finished_semaphores
+            .iter()
+            .for_each(|s| self.device.destroy_semaphore(*s, None));
+        self.data
+            .image_available_semaphores
+            .iter()
+            .for_each(|s| self.device.destroy_semaphore(*s, None));
 
         self.device
             .destroy_command_pool(self.data.command_pool, None);
@@ -211,6 +247,8 @@ struct AppData {
     framebuffers: Vec<vk::Framebuffer>,
     command_pool: vk::CommandPool,
     command_buffers: Vec<vk::CommandBuffer>,
-    image_available_semaphore: vk::Semaphore,
-    render_finished_semaphore: vk::Semaphore,
+    image_available_semaphores: Vec<vk::Semaphore>,
+    render_finished_semaphores: Vec<vk::Semaphore>,
+    in_flight_fences: Vec<vk::Fence>,
+    images_in_flight: Vec<vk::Fence>,
 }
