2b79683a7e524be32cc1873f2f0b3bd89fc63b78
Marcus Haukelid Larsen <mhl@haukelid.dk>
2024-01-16 12:24:51 +0000

    Implementation of model loading

--- src/model/model.rs
+++ src/model/model.rs
@@ -9,10 +9,31 @@ use cgmath::{vec2, vec3};
 use crate::vertex::vertex;
 use crate::AppData;
 
-pub fn load_model(data: &mut AppData) -> Result<()> {
-    // Model
+impl PartialEq for vertex::Vertex {
+    fn eq(&self, other: &Self) -> bool {
+        self.pos == other.pos && self.color == other.color && self.tex_coord == other.tex_coord
+    }
+}
 
-    let mut reader = BufReader::new(File::open("/home/mhl/vulkanalia_tutorial/src/texture/resources/viking_room.obj")?);
+impl Eq for vertex::Vertex {}
+
+impl Hash for vertex::Vertex {
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.pos[0].to_bits().hash(state);
+        self.pos[1].to_bits().hash(state);
+        self.pos[2].to_bits().hash(state);
+        self.color[0].to_bits().hash(state);
+        self.color[1].to_bits().hash(state);
+        self.color[2].to_bits().hash(state);
+        self.tex_coord[0].to_bits().hash(state);
+        self.tex_coord[1].to_bits().hash(state);
+    }
+}
+
+pub fn load_model(data: &mut AppData) -> Result<()> {
+    let mut reader = BufReader::new(File::open(
+        "/home/mhl/vulkanalia_tutorial/src/texture/resources/viking_room.obj",
+    )?);
 
     let (models, _) = tobj::load_obj_buf(
         &mut reader,
@@ -23,7 +44,7 @@ pub fn load_model(data: &mut AppData) -> Result<()> {
         |_| Ok(Default::default()),
     )?;
 
-    // Vertices / Indices
+    let mut unique_vertices = HashMap::new();
 
     for model in &models {
         for index in &model.mesh.indices {
@@ -39,14 +60,18 @@ pub fn load_model(data: &mut AppData) -> Result<()> {
                 color: vec3(1.0, 1.0, 1.0),
                 tex_coord: vec2(
                     model.mesh.texcoords[tex_coord_offset],
-                    model.mesh.texcoords[tex_coord_offset + 1],
+                    1.0 - model.mesh.texcoords[tex_coord_offset + 1],
                 ),
             };
 
-            data.vertex_data.vertices.push(vertex);
-            data.vertex_data
-                .indices
-                .push(data.vertex_data.indices.len() as u32);
+            if let Some(index) = unique_vertices.get(&vertex) {
+                data.vertex_data.indices.push(*index as u32);
+            } else {
+                let index = data.vertex_data.vertices.len();
+                unique_vertices.insert(vertex, index);
+                data.vertex_data.vertices.push(vertex);
+                data.vertex_data.indices.push(index as u32);
+            }
         }
     }
 
